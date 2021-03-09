8b9e5ff6ac72942feabe04fa8728c2a26a422d2f
YuryevGN <yuryev@strim-tech.com>
2021-03-09 16:13:19 +0300

    Refactoring

--- methods.c
+++ methods.c
@@ -1,9 +1,15 @@
 
-#include "methods.h"
 #include "http.h"
 
+typedef struct
+{
+  const uint8_t * Name;
+  uint32_t        NameLength;
 
-const MH_MethodTable_t MH_MethodTable[MH_Method_LastIndex] = {
+} MH_MethodTable_t;
+
+
+static const MH_MethodTable_t MH_MethodTable[MH_Method_LastIndex] = {
 
     [MH_Method_GET]   = {MH_NAME_AND_LENGTH("GET")},
     [MH_Method_POST]  = {MH_NAME_AND_LENGTH("POST")}
@@ -18,4 +24,14 @@ const uint8_t * s_MH_GetMethodName(MH_Method_t method)
   }
 
   return "Unknown";
+}
+
+uint32_t u32_MH_GetMethodNameLength(MH_Method_t method)
+{
+  if (method < MH_Method_LastIndex)
+  {
+    return MH_MethodTable[method].NameLength;
+  }
+
+  return 0;
 }
